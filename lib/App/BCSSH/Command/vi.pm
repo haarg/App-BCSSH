63925467f965f3e9f048f2bdf8651ed53fa0f02c
Graham Knop <haarg@haarg.org>
2013-01-25 10:46:46 -0500

    auth key support for vi command

--- lib/App/BCSSH/Command/vi.pm
+++ lib/App/BCSSH/Command/vi.pm
@@ -9,11 +9,14 @@ use App::BCSSH::Message;
 sub run {
     my $class = shift;
     my $agent = $ENV{SSH_AUTH_SOCK} or exec 'vi', @_;
-    my ($file) = @_;
-    $file or die "file required\n";
-    my $full_file = Cwd::abs_path($file);
-    my ($type) = App::BCSSH::Client::send($agent, BCSSH_EDIT, $full_file);
-    return $type == BCSSH_SUCCESS;
+    my $auth_key = $ENV{LC_BCSSH_KEY} || '';
+    my @files = @_;
+    @files or die "At least one file must be specified!\n";
+    for my $file (@files) {
+        $file = File::Spec->rel2abs($file);
+    }
+    my ($type) = App::BCSSH::Client::send($agent, BCSSH_EDIT, $auth_key, @files);
+    return $type && $type == BCSSH_SUCCESS;
 }
 
 1;
