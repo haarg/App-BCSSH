2de4f0022a84247ae68a0cf68e7bac02b50696da
Graham Knop <haarg@haarg.org>
2013-01-25 10:44:44 -0500

    Better handling of ssh exit and adding auth option

--- lib/App/BCSSH/Command/ssh.pm
+++ lib/App/BCSSH/Command/ssh.pm
@@ -1,6 +1,6 @@
 package App::BCSSH::Command::ssh;
 use Moo;
-
+use Sub::Quote;
 use POSIX ":sys_wait_h";
 use App::BCSSH::Message;
 use App::BCSSH::Proxy;
@@ -9,47 +9,87 @@ use App::BCSSH::Options;
 
 with Options(
     -config => {permute => 0},
+    auth => 'auth!',
 );
+
+has agent_path => ( is => 'ro', default => quote_sub q[ $ENV{SSH_AUTH_SOCK} ] );
+has host => ( is => 'ro', lazy => 1, default => quote_sub q{ $_[0]->find_host($_[0]->args) } );
+has gvim => ( is => 'ro', default => quote_sub q{ 'gvim' });
+has auth => ( is => 'ro' );
+has auth_key => (
+    is => 'ro',
+    lazy => 1,
+    default => quote_sub q[ join '', map { chr(32+int(rand(96))) } (1..20) ],
+);
+has proxy => ( is => 'lazy' );
 
 sub run {
     my $self = shift;
     my $args = $self->args;
-    my $agent_path = $ENV{SSH_AUTH_SOCK};
-    my $host = $self->find_host($args);
+    my $host = $self->host;
     if (! $host) {
+        warn "Can't calculate hostname. Disabling.\n";
         exec 'ssh', @$args;
     }
-    if ($self->is_bcssh_agent($agent_path)) {
-        die "not supported";
+    my $agent_path = $self->agent_path;
+    if ($agent_path && $self->is_bcssh_agent($agent_path)) {
+        warn "Not able to re-proxy to bcssh. Disabling.\n";
+        exec 'ssh', @$args;
     }
-    my $proxy = App::BCSSH::Proxy->new(
-        agent_path => $agent_path,
+    my $proxy = $self->proxy;
+    $ENV{SSH_AUTH_SOCK} = $proxy->socket_path;
+    if ($self->auth) {
+        $ENV{LC_BCSSH_AUTH} = $self->auth_key;
+    }
+    my $guard = $self->proxy_guard;
+    exit system('ssh', @$args);
+}
+
+sub _build_proxy {
+    my $self = shift;
+    my $host = $self->host;
+    my $gvim = $self->gvim;
+    my $auth_key = $self->auth && $self->auth_key;
+    my $check_key = $auth_key ? sub { die if $_[0] ne $auth_key } : sub () {};
+    return App::BCSSH::Proxy->new(
+        agent_path => $self->agent_path,
         handlers => {
-            (BCSSH_QUERY) => sub {
-                $_[0]->(BCSSH_SUCCESS);
-            },
-            (BCSSH_EDIT) => sub {
-                my ($send, $file) = @_;
-                my $file_path = "scp://$host/$file";
-                fork or exec 'gvim', '--', $file_path;
-                $send->(BCSSH_SUCCESS);
-            },
+            $host ? (
+                (BCSSH_QUERY) => sub {
+                    my ($send, $key) = @_;
+                    $check_key->($key);
+                    $send->(BCSSH_SUCCESS);
+                },
+                (BCSSH_EDIT) => sub {
+                    my ($send, $key, $file) = @_;
+                    $check_key->($key);
+                    my $file_path = "scp://$host/$file";
+                    fork or exec $gvim, '--', $file_path;
+                    $send->(BCSSH_SUCCESS);
+                },
+            ) : (
+                map { $_ => sub { $_[0]->(SSH_AGENT_FAILURE) } } (
+                    BCSSH_QUERY,
+                    BCSSH_EDIT,
+                ),
+            ),
         },
     );
-    my $proxy_sock = $proxy->socket_path;
-    $ENV{SSH_AUTH_SOCK} = $proxy_sock;
-    my $child = fork;
+}
+
+sub proxy_guard {
+    my ($self, $child_cb) = @_;
+    my $proxy = $self->proxy;
+    my $child = open my $fh, '|-';
     if (!$child) {
         chdir '/';
-#        open STDIN, '<', '/dev/null';
-#        open STDERR, '>', '/dev/null';
-#        open STDERR, '>', '/dev/null';
-        $proxy->proxy;
+        $0 = 'bcssh proxy';
+        open STDOUT, '>', '/dev/null';
+        open STDERR, '>', '/dev/null';
+        $proxy->(\*STDIN);
         exit;
     }
-    my $out = system 'ssh', @$args;
-    waitpid($child, WNOHANG) != -1 and kill 'HUP', $child;
-    exit $out;
+    return $fh;
 }
 
 sub find_host {
