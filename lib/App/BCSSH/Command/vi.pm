e685c4e964bf6ee6fbf8974ba28beab1907a5cff
Graham Knop <haarg@haarg.org>
2013-01-26 04:46:38 -0500

    objectify vi command and use client role

--- lib/App/BCSSH/Command/vi.pm
+++ lib/App/BCSSH/Command/vi.pm
@@ -1,21 +1,23 @@
 package App::BCSSH::Command::vi;
-use strict;
-use warnings;
+use Moo;
+use App::BCSSH::Options;
+with Options();
+with 'App::BCSSH::Client';
 
-use Cwd;
-use App::BCSSH::Message;
+use File::Spec;
+use App::BCSSH::Message ':message_types';
+
+has 'wait' => (is => 'ro', coerce => sub { $_[0] ? 1 : 0 }, arg_spec => 'f');
 
 sub run {
-    my $class = shift;
-    my $agent = $ENV{SSH_AUTH_SOCK} or exec 'vi', @_;
-    my $auth_key = $ENV{LC_BCSSH_KEY} || '';
+    my $self = shift;
     my @files = @_;
     @files or die "At least one file must be specified!\n";
     for my $file (@files) {
         $file = File::Spec->rel2abs($file);
     }
-    my ($type) = App::BCSSH::Message::send_message($agent, BCSSH_EDIT, $auth_key, @files);
-    return $type && $type == BCSSH_SUCCESS;
+    my $result = $self->message(BCSSH_EDIT, $self->wait, @files);
+    return $result == BCSSH_SUCCESS;
 }
 
 1;
