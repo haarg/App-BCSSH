6669ff55e173611f84e2ac1da91215b7668e3732
Graham Knop <haarg@haarg.org>
2013-04-09 15:43:15 -0400

    add browse command

---
+++ lib/App/BCSSH/Command/browse.pm
@@ -0,0 +1,28 @@
+package App::BCSSH::Command::browse;
+use Moo;
+use App::BCSSH::Options;
+use App::BCSSH::Client;
+with Options;
+with Client('browse');
+
+use File::Spec;
+
+sub run {
+    my $self = shift;
+    my @urls = @{ $self->args };
+    @urls or die "At least one url must be specified!\n";
+    $self->command({ urls => \@urls });
+}
+
+1;
+__END__
+
+=head1 NAME
+
+App::BCSSH::Command::browse - Open a URL in the user's local browser
+
+=head1 SYNOPSIS
+
+    bcssh browse -- http://www.example.com/
+
+=cut
---
+++ lib/App/BCSSH/Proxy/Handler/Browse.pm
@@ -0,0 +1,25 @@
+package App::BCSSH::Proxy::Handler::Browse;
+use Moo;
+use Browser::Open qw(open_browser);
+
+with 'App::BCSSH::Proxy::Handler';
+
+has browser => (is => 'ro');
+has browse => (is => 'lazy', init_arg => undef);
+sub _build_browse {
+    my $self = shift;
+    my $browser = $self->browser;
+    $browser ? sub { system $browser, @_ } : \&open_browser;
+}
+
+sub handle {
+    my ($self, $send, $args) = @_;
+    my $urls = $args->{urls};
+
+    for my $url (@$urls) {
+        $self->browse->($url);
+    }
+    $send->();
+}
+
+1;
