56901ff5e4ad14ab94df44089acde8d8b2663d07
Graham Knop <haarg@haarg.org>
2013-04-09 16:48:31 -0400

    remove needless forking from help command

--- lib/App/BCSSH/Command/help.pm
+++ lib/App/BCSSH/Command/help.pm
@@ -28,48 +28,39 @@ sub help_for_package {
     my $pod = module_content($package, { found => \%INC });
     my $command = package_to_command($package);
 
-    my $pid = fork;
-    if ( !$pid ) {
-        # perldoc will try to drop privs anyway, so do it ourselves so the
-        # temp file has the correct owner
-        Pod::Perldoc->new->drop_privs_maybe;
+    # perldoc will try to drop privs anyway, so do it ourselves so the
+    # temp file has the correct owner
+    Pod::Perldoc->new->drop_privs_maybe;
 
-        my $pod_name = $command ? "bcssh-$command" : $package;
-        my $pod_file = $pod_name;
-        $pod_file =~ s/::/-/g;
-        my $section = $command ? 1 : 3;
-        my $tmpdir = File::Temp->newdir( TMPDIR => 1 );
-        my $out_file = File::Spec->catfile($tmpdir, $pod_file);
-        open my $out, '>', $out_file;
-        print {$out} $pod;
-        close $out;
-        {
-            no warnings 'redefine';
-            # fix width handling
-            *Pod::Perldoc::ToMan::is_linux = sub () { 1 };
-            # silence groff warning
-            *Pod::Perldoc::ToMan::warn = sub {};
-            # fix option passing
-            *Pod::Perldoc::ToMan::_get_podman_switches = sub {
-                my $self = shift;
-                return map {; $_ => $self->{$_} } grep !m/^_/s, keys %$self;
-            };
-        }
-        @ARGV = (
-            -o => 'Man',
-            -w => "name:$pod_name",
-            -w => "section:$section",
-            -w => 'center:',
-            '-F' => $out_file,
-        );
-        exit Pod::Perldoc->run;
+    my $pod_name = $command ? "bcssh-$command" : $package;
+    my $pod_file = $pod_name;
+    $pod_file =~ s/::/-/g;
+    my $section = $command ? 1 : 3;
+    my $tmpdir = File::Temp->newdir( TMPDIR => 1 );
+    my $out_file = File::Spec->catfile($tmpdir, $pod_file);
+    open my $out, '>', $out_file;
+    print {$out} $pod;
+    close $out;
+    {
+        no warnings qw(redefine once);
+        # fix width handling
+        *Pod::Perldoc::ToMan::is_linux = sub () { 1 };
+        # silence groff warning
+        *Pod::Perldoc::ToMan::warn = sub {};
+        # fix option passing
+        *Pod::Perldoc::ToMan::_get_podman_switches = sub {
+            my $self = shift;
+            return map {; $_ => $self->{$_} } grep !m/^_/s, keys %$self;
+        };
     }
-    waitpid $pid, 0;
-
-    if ($?) {
-        die "Error displaying help!\n";
-    }
-    return;
+    @ARGV = (
+        -o => 'Man',
+        -w => "name:$pod_name",
+        -w => "section:$section",
+        -w => 'center:',
+        '-F' => $out_file,
+    );
+    exit Pod::Perldoc->run;
 }
 
 1;
